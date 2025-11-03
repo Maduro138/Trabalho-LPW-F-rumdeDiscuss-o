005d9ff56de07911b5e96be4e652b22145da685e
cristianoamorim <cristianoamorim2009@gmail.com>
2025-11-03 20:13:00 -0300

    Cristiano
    Fiz o html da página inicial.

--- MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
+++ MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
@@ -1,17 +1,3 @@
-document.addEventListener("DOMContentLoaded",()=>{
 
-let Bemvindo = document.getElementById("Bemvindo")
-const nomedousuario = localStorage.getItem("nomedousuario")
 
-if (Bemvindo) {
-    Bemvindo.innerHTML = 'Bem vindo '+`${nomedousuario}`+ ' :)'
-
-}
-else if(Bemvindo=== "null" ){
-    Bemvindo.innerHTML = 'Bem vindo (a) :)'
-}
-else{
-
-}
-})
 
