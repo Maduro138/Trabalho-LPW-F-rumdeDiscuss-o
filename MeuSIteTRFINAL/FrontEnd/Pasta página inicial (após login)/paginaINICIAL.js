bb8b83b1e66b3c004b6a148d543856cafd575334
Arthur Maduro <arthur11maduro@gmail.com>
2025-11-04 19:20:59 -0300

    Servidor rodando
    👍

--- MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
+++ MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded",()=>{
 
+let Bemvindo = document.getElementById("Bemvindo")
+const nomedousuario = localStorage.getItem("nomedousuario")
 
+if (Bemvindo) {
+    Bemvindo.innerHTML = 'Bem vindo '+`${nomedousuario}`+ ' :)'
+
+}
+else if(Bemvindo=== "null" ){
+    Bemvindo.innerHTML = 'Bem vindo (a) :)'
+}
+else{
+
+}
+})
 
