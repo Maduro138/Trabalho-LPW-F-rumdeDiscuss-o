eb9a7c55f2e29db7379173fb25b6475ab7040fd1
cristianoamorim <cristianoamorim2009@gmail.com>
2025-11-06 20:46:06 -0300

    Cristiano
    Alterei o javascript da página inicial

---
+++ MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+  let Bemvindo = document.getElementById("Bemvindo");
+  let login = document.getElementById("login");
+  let nomedousuario = localStorage.getItem("nomedousuario");
+
+  if (nomedousuario) {
+    nomedousuario = nomedousuario.trim();
+    
+  } 
+  
+  if (nomedousuario === "") {
+    nomedousuario = "Convidado";
+    
+  }
+
+  if (Bemvindo) {
+    Bemvindo.textContent = `Bem vindo ${nomedousuario} :)`;
+  }
+
+  else {
+      login = "";
+    }
+});
