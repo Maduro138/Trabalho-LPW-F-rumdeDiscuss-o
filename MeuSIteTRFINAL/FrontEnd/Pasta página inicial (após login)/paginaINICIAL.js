1028548c3715c32d10650b8bb0fa16ba49c0db23
cristianoamorim <cristianoamorim2009@gmail.com>
2025-11-05 22:14:05 -0300

    beta 0.1
    Colocamos novas fontes na página inicial e mudamos o css e javascript

--- MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
+++ MeuSIteTRFINAL/FrontEnd/Pasta página inicial (após login)/paginaINICIAL.js
@@ -1,3 +1,27 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+let Bemvindo = document.getElementById("Bemvindo")
+let nomedousuario = localStorage.getItem("nomedousuario") //Colocar para tirar espaços no nome do usuário
+let login = document.getElementById("login").value
+
+if(nomedousuario === null){
+
+    nomedousuario = 'Convidado'
+}
+
+
+if (Bemvindo) {   
+    Bemvindo.innerHTML = 'Bem vindo '+`${nomedousuario}`+ ' :)'
+    login.innerHTML=""
+}
+else if(Bemvindo=== "null" ){
+    Bemvindo.innerHTML = 'Bem vindo (a) :)'
+}
+else{
+
+}
+})
+
 
 
 
