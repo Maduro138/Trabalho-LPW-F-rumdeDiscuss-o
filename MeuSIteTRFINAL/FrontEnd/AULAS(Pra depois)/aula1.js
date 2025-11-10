e448a0146a51c1d422b4f71115accf485061709a
cristianoamorim <cristianoamorim2009@gmail.com>
2025-11-10 18:54:03 -0300

    Cristiano
    Fiz o css da aula 1

--- MeuSIteTRFINAL/FrontEnd/AULAS(Pra depois)/aula1.js
+++ MeuSIteTRFINAL/FrontEnd/AULAS(Pra depois)/aula1.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll('[data-copiar]').forEach(botao => {
+  botao.onclick = () => {
+    const alvo = document.querySelector(botao.dataset.copiar);
+    navigator.clipboard.writeText(alvo.innerText);
+    botao.textContent = 'Copiado!';
+    setTimeout(() => botao.textContent = 'Copiar', 1000);
+  };
+});
